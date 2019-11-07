180d6153dbe482a228eaabd3156656634e12b30f
hoangphong <hoangphong24796@gmail.com>
2019-11-07 14:42:56 +0700

    Initial commit

--- app/src/main/java/com/phong/hocluutrangthaisharepreferences/MainActivity.java
+++ app/src/main/java/com/phong/hocluutrangthaisharepreferences/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -36,5 +37,29 @@ public class MainActivity extends AppCompatActivity {
         dsChuoi.add("Bình Nguyên Royal");
         editor.putStringSet("dsChuoi", dsChuoi);
         editor.commit();//Xác nhận lưu
+    }
+
+    public void XuLyDocTrangThai(View view) {
+        SharedPreferences preferences = getSharedPreferences(tenSharePreferences, MODE_PRIVATE);
+        boolean b = preferences.getBoolean("b", false);//nếu ko tìm thấy thì lấy value mặc định là false
+        float f = preferences.getFloat("f", 0.0f);
+        int i = preferences.getInt("i", 0);
+        long l = preferences.getLong("l", 0l);
+        String s = preferences.getString("s","");
+        Set<String> dsChuoi = preferences.getStringSet("dsChuoi", null);
+        //Hiển thị lên TextView:
+        TextView txtTrangThai = (TextView) findViewById(R.id.txtTrangThai);
+        txtTrangThai.append("b = " + b + "\n");
+        txtTrangThai.append("f = " + f + "\n");
+        txtTrangThai.append("i = " + i + "\n");
+        txtTrangThai.append("l = " + l + "\n");
+        txtTrangThai.append("s = " + s + "\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String x : dsChuoi)
+        {
+            stringBuilder.append("dsChuoi = " + x + "\n");
+        }
+        txtTrangThai.append("Danh sách từ set: \n");
+        txtTrangThai.append(stringBuilder.toString());
     }
 }
